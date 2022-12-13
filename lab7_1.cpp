8a309d40fb3f1c57280176084b69072506b017e1
thanabordeee <jejekup00123@hotmail.co.th>
2022-12-13 17:17:18 +0700

    eieizaza

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -34,8 +34,20 @@ string func3(string x){
 }
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+	string text;
+    cout << "Input text: ";
+	cin >> text;
+    cout << "Reversed text: " << func1(text);
+	if(func2(text) == func1(func2(text))){
+    cout << "\nPalindrome: Yes";
+	}
+	else if (func3(text) == func1(func3(text)))
+	{
+		cout << "\nPalindrome: Yes";
+	}
+	else{
+		 cout << "\nPalindrome: No";
+	}
+
     return 0;
 }
